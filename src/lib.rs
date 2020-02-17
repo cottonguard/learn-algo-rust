19f036ecb54b8ba762f4b4564ba19feb28597061
cotton <mail@example.com>
2020-02-17 18:28:45 +0900

    extgcd

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,2 @@
+pub mod number;
 pub mod string;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
---
+++ src/number/mod.rs
@@ -0,0 +1,26 @@
+use num::Num;
+
+/// extgcd(a, b) -> (gcd(a, b), x, y)
+/// satisfies ax + by = gcd(a, b)
+pub fn extgcd<T>(a: T, b: T) -> (T, T, T)
+where
+    T: Copy + Num,
+{
+    if b != T::zero() {
+        let (g, y, x) = extgcd(b, a % b);
+        (g, x, y - a / b * x)
+    } else {
+        (a, T::one(), T::zero())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn extgcd_test() {
+        assert_eq!(extgcd(111, 30), (3, 3, -11));
+        assert_eq!(extgcd(1071, 1029), (21, -24, 25));
+        assert_eq!(extgcd(1, 1), (1, 0, 1));
+    }
+}
