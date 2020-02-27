d13b6f17674e3d2661ff764f08593e02c633979f
cotton <mail@example.com>
2020-02-28 01:56:54 +0900

    fix swag

--- src/structures/swag.rs
+++ src/structures/swag.rs
@@ -1,26 +1,22 @@
-use std::mem::swap;
-
 pub struct Swag<T, F> {
     f: F,
-    head: Vec<T>,
     f_head: Vec<T>,
     tail: Vec<T>,
-    f_tail: Vec<T>,
+    f_tail: Option<T>,
 }
 
 impl<T: Clone, F: Fn(&T, &T) -> T> Swag<T, F> {
     pub fn new(f: F) -> Self {
         Self {
             f,
-            head: Vec::new(),
             f_head: Vec::new(),
             tail: Vec::new(),
-            f_tail: Vec::new(),
+            f_tail: None,
         }
     }
 
     pub fn push(&mut self, x: T) {
-        self.f_tail.push(if let Some(y) = self.f_tail.last() {
+        self.f_tail = Some(if let Some(y) = self.f_tail.as_ref() {
             (self.f)(y, &x)
         } else {
             x.clone()
@@ -28,17 +24,22 @@ impl<T: Clone, F: Fn(&T, &T) -> T> Swag<T, F> {
         self.tail.push(x);
     }
 
-    pub fn pop(&mut self) -> Option<T> {
-        if self.head.is_empty() {
-            swap(&mut self.head, &mut self.tail);
-            swap(&mut self.f_head, &mut self.f_tail);
+    pub fn pop(&mut self) {
+        if self.f_head.is_empty() {
+            for x in self.tail.drain(..).rev() {
+                self.f_head.push(if let Some(y) = self.f_head.last() {
+                    (self.f)(&x, y)
+                } else {
+                    x
+                });
+            }
+            self.f_tail = None;
         }
         self.f_head.pop();
-        self.head.pop()
     }
 
     pub fn fold(&self) -> Option<T> {
-        match (self.f_head.last(), self.f_tail.last()) {
+        match (self.f_head.last(), self.f_tail.as_ref()) {
             (Some(x), Some(y)) => Some((self.f)(x, y)),
             (Some(x), None) => Some(x.clone()),
             (None, Some(y)) => Some(y.clone()),
@@ -47,14 +48,13 @@ impl<T: Clone, F: Fn(&T, &T) -> T> Swag<T, F> {
     }
 
     pub fn len(&self) -> usize {
-        self.head.len() + self.tail.len()
+        self.f_head.len() + self.tail.len()
     }
 
     pub fn is_empty(&self) -> bool {
-        self.head.is_empty() && self.tail.is_empty()
+        self.f_head.is_empty() && self.tail.is_empty()
     }
 }
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -68,14 +68,14 @@ mod tests {
         swag.push(3);
         assert_eq!(swag.fold(), Some(3));
         assert_eq!(swag.len(), 2);
-        assert_eq!(swag.pop(), Some(3));
-        assert_eq!(swag.fold(), Some(5));
+        swag.pop();
+        assert_eq!(swag.fold(), Some(3));
         swag.push(4);
+        assert_eq!(swag.fold(), Some(3));
+        swag.pop();
         assert_eq!(swag.fold(), Some(4));
-        assert_eq!(swag.pop(), Some(5));
-        assert_eq!(swag.fold(), Some(4));
-        assert_eq!(swag.pop(), Some(4));
+        swag.pop();
         assert_eq!(swag.fold(), None);
-        assert_eq!(swag.pop(), None);
+        swag.pop();
     }
 }
